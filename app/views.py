fe2002092568ef6ccea8387a619ea21dd852f548
ookuboc5399 <ankle0614@gmail.com>
2023-07-23 21:37:30 +0900

    firstcommit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = "app/index.html"
+    login_url = '/accounts/login/'
